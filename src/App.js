7dd1db7ee2551472a8c9b22e208dc3c0a4aa41f0
hakeem235 <hakeemkoo@Gmail.com>
2021-11-21 06:45:48 -0500

    ADD Detail  Home Login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/detail" element={<Detail />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
